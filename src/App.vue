68919a5808c2591145d1eac3a6c4744f29bc692f
Allen Liu <lhard@qq.com>
2019-04-10 14:34:43 +0800

    simple

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>单向数据流示例：</h3>
+    <simple></simple>
+    <hr/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import simple from './components/simple.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    simple
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/simple.vue
@@ -0,0 +1,31 @@
+<!-- view -->
+<template>
+  <div class="simple">
+    <button @click="increment">自加</button>
+    <span>num:<b>{{num}}</b></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'simple',
+  // state
+  data() {
+    return {
+      num: 1,
+    }
+  },
+  // actions
+  methods: {
+    increment() {
+      this.num ++;
+    },
+  },
+}
+// 单向数据流：
+// state，驱动应用的数据源；
+// view，以声明方式将 state 映射到视图；
+// actions，响应在 view 上的用户输入导致的状态变化。
+
+// 地址：https://vuex.vuejs.org/zh/
+</script>
